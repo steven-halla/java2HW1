0961f0d1150ed515919d2c2eedcef4cdd8442a0f
steven-halla <69405488+steven-halla@users.noreply.github.com>
2023-04-06 17:21:10 -0700

    first

---
+++ src/ForLoop.java
@@ -0,0 +1,7 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        for(int number = 0; number <= 10; number++) {
+            System.out.println(number);
+        }
+    }
+}
